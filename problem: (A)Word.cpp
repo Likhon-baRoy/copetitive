6822dd6c23875fb1cded7c7f692f152cbf3eff7e
Likhon-baRoy <61086375+Likhon-baRoy@users.noreply.github.com>
2020-11-21 15:12:52 +0600

    Create problem: (A)Word.cpp

---
+++ problem: (A)Word.cpp
@@ -0,0 +1,18 @@
+https://codeforces.com/problemset/problem/59/A   
+
+ #include <bits/stdc++.h>
+    using namespace std;
+    int main()
+    {
+    	string s;cin>>s;
+    	int uc=0 , lc=0;
+    	for(char c:s)
+    	{
+    		(isupper(c)?uc:lc)++;
+    	}
+    	for(char c:s)
+    	{
+    		cout<<char(lc>=uc?tolower(c):toupper(c));
+    	}
+    	return 0;
+    }
